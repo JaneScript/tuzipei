5e12c28ba906ab18c866cc13e43f94c612e1d85b
humingchao <humingchao@mana.com>
2017-05-10 14:44:56 +0800

    merge

--- resources/assets/js/home.js
+++ resources/assets/js/home.js
@@ -1,3 +1,4 @@
+
 $(function() {
 	/********加载公共头部和尾部方法********/
 	$('#header').load('./common/header.html',function() {
@@ -5,7 +6,17 @@ $(function() {
 	})
 	$('#footer').load('./common/footer.html')
 	
-	/********鼠标移入移出显示/隐藏二维码事件********/
+	/********鼠标移入移出显示/隐藏二维码********/
+	showQRCode();
+	
+	/********获取文章数据********/
+	getList(1,10);
+})
+
+
+
+
+function showQRCode() {//鼠标移入显示二维码
 	var path = ['wechat-logo','weibo-logo','email-logo'];
 	var pathActive = ['wechat-logo2','weibo-logo2','email-logo2'];
 	$('.qr-code').children('p').mouseover(function() {
@@ -40,60 +51,103 @@ $(function() {
 			'display': 'none',
 		})
 	})
-	
-	
-	/********获取文章数据方法********/
-	
+}
+
+function getList(page,limit) {//获取数据后插入文档显示
 	var params = {
-		page : 1,
-		limit : 10
+		page : page,
+		limit : limit
 	}
-	getList('GET', 'http://203.110.178.189:8090/weixin.php?check=getlist', params);
-	
-	function getList(type, url, params) {
-		$.ajax({
-            type: type,
-            url: url,
-            data: params,
-            beforeSend: function() {
-                console.log('loading...');
-            },
-            
-            success: function(res){
-            	
-        		var res = JSON.parse( res );
-        		
-        		var len = res.data.length;
-        		if(len) {
-	        		for(var i=0; i<len; i++) {
-	        			console.log(res.data[i].article_title);
-	        			$('.main-content').append(
-	        				'<dl>'+
-								'<dt>'+
-									'<img src='+res.data[i].article_thumbnail+'/>'+
-								'</dt>'+
-								'<dd>'+
-									'<h3>'+res.data[i].article_title+'</h3>'+
-									'<p>'+res.data[i].article_brief+'</p>'+
-									'<div>'+
-										'<span class="public-num">公众号</span>'+
-										'<span class="date">'+res.data[i].article_publish_time+'</span>'+
+	var data = getAjax('GET', 'http://203.110.178.189:8090/weixin.php?check=getlist', params);
+	var len = data.data.length;
+	if(len) {
+		for(var i=0; i<len; i++) {
+			if(i < 4) {
+				$('.main-content').append(
+    				'<dl>'+
+						'<dt>'+
+							'<img src='+data.data[i].article_thumbnail+'/>'+
+						'</dt>'+
+						'<dd>'+
+							'<h3>'+data.data[i].article_title+'</h3>'+
+							'<p>'+data.data[i].article_brief+'</p>'+
+							'<div>'+
+								'<span class="public-num">公众号</span>'+
+								'<span class="date">'+data.data[i].article_publish_time+'</span>'+
 //										'<span class="share">点赞(2134)</span>'+
 //										'<span class="comments">阅读(3423)</span>'+
-									'</div>'+
-								'</dd>'+
-							'</dl>'
-	        			)
-	        		}
-        		}
-        		
-        		console.log(res.totalpages)
-            },
-            
-            error: function(res) {
-            	console.log('err',res);
-            }
-       	});
+							'</div>'+
+						'</dd>'+
+					'</dl>'
+				)
+			}
+			if(i < 6) {
+				$('.hotTitle-right').append(
+					'<dl>'+
+						'<dt></dt>'+
+						'<dd>'+data.data[i].article_title+'</dd>'+
+					'</dl>'
+				)
+			}
+		}
+		
+		$('.main-content , .hotTitle-right').on('click', 'dl', function() {
+			var index = $(this).index();
+			localStorage.setItem("article_content",data.data[index].article_content);
+			location.href = 'detail.html';
+		})
 	}
 	
-})
+	//点击排行和最新动态事件
+	var _clickLi = false;
+	$('.nav-tabs').on('click', 'li', function() {
+		var index = $(this).index();
+		$('.hotTitle-right').html('');
+		if(index == 0) {
+			for(var i=0; i<len; i++) {
+				if(i%2==1 || i==8) {
+					$('.hotTitle-right').append(
+						'<dl>'+
+							'<dt></dt>'+
+							'<dd>'+data.data[i].article_title+'</dd>'+
+						'</dl>'
+					)
+				}
+			}
+		} else if(index == 1) {
+			for(var i=0; i<len; i++) {
+				if(i < 6) {
+					$('.hotTitle-right').append(
+						'<dl>'+
+							'<dt></dt>'+
+							'<dd>'+data.data[i].article_title+'</dd>'+
+						'</dl>'
+					)
+				}
+			}
+		}
+	})
+}
+
+function getAjax(type, url, params) {//ajax请求数据
+	var result = '';
+	$.ajax({
+        type: type,
+        url: url,
+        data: params,
+        async: false,
+        beforeSend: function() {
+            console.log('loading...');
+        },
+        success: function(res) {
+        	var res = JSON.parse(res);
+        	if (res.err == "0") {
+        		result = res;
+        	}
+        },
+        error: function(res) {
+        	console.log(res);
+        } 
+   });
+   	return result;
+}
